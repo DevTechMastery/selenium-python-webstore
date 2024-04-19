f356b8958f291c770a1ffb5a2e5c79e426896537
devtech <devtechmastery@gmail.com>
2024-04-18 21:47:25 -0700

    Added locators to CartPageLocators.py for cart page elements

--- src/pages/locators/CartPageLocators.py
+++ src/pages/locators/CartPageLocators.py
@@ -6,3 +6,7 @@ class CartPagesLocators:
 
     # TBD - Add locators for Cart page
     # PRODUCT_NAMES_IN_CART = (By.CSS_SELECTOR, '')
+
+    LINK_ADD_COUPON = (By.CSS_SELECTOR, 'a.wc-block-components-totals-coupon-link')
+    COUPON_FIELD = (By.ID, 'wc-block-components-totals-coupon__input-0')
+    APPLY_COUPON_BUTTON = (By.CSS_SELECTOR,'button.wc-block-components-totals-coupon__button')

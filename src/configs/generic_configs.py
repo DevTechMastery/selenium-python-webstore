b2c6923211edf762c3a2c8139b75d2b19e8084d5
devtech <devtechmastery@gmail.com>
2024-05-23 16:38:24 -0700

    Updated generic_configs.py

--- src/configs/generic_configs.py
+++ src/configs/generic_configs.py
@@ -1,5 +1,10 @@
-
+# generic_configs.py
+# Contains general configuration constants used throughout the application.
 class GenericConfigs:
+    """
+        Stores static configuration values that do not change across different parts of the application.
+        """
 
+    # Coupon code for a free offer, applicable universally across all users.
     FREE_COUPON = 'FRIENDS100'
 
